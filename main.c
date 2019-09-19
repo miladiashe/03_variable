acb07215463e39f8935d6be8f0aba2c64b36f57a
miladiashe <55428028+miladiashe@users.noreply.github.com>
2019-09-19 15:30:59 +0900

    09/19 class final
    witn no play

--- main.c
+++ main.c
@@ -6,16 +6,12 @@
 int main(int argc, char *argv[]) {
 	
 	int x;
-	short xs;
-	long xl;
-	char y;
+	short y;
 	float z;
 	double a;
 	
 	printf("byte of int : %d\n", sizeof(x) ); 
-	printf("byte of short int : %d\n", sizeof(xs) );
-	printf("byte of long int : %d\n", sizeof(xl) );
-	printf("byte of char : %d\n", sizeof(y) );
+	printf("byte of short : %d\n", sizeof(y) );
 	printf("byte of float : %d\n", sizeof(z) );
 	printf("byte of double : %d\n", sizeof(a) );
 	
